60a71c524b215a37b98aa8be4780d7bf2d7763b4
Steffen Hankiewicz <steffen.hankiewicz@intranda.com>
2017-11-27 19:41:49 +0100

    added more documentation and updated scripts

--- goobi-plugin-step-olr-newspaper-recognizer/doc/README.md
+++ goobi-plugin-step-olr-newspaper-recognizer/doc/README.md
@@ -40,3 +40,37 @@ Konkreter Aufruf für das Demomaterial innerhalb dieses Eclipse-Projekts:
 
 	python2 predict_cnn.py material/demmta_1911 tmp_demmta_1911 demmta_1911.json
 	
+# Installation innerhalb eines Goobi Workflows
+
+## Kopieren der richtigen Dateien:
+
+Die beiden Konfigurationsdateien `goobi_opac.xml` und `goobi_projects.xml` aus dem doc-Ordner in den Goobi-config-Pfad kopieren. Also üblicherweise in eines dieser beiden Verzeichnisse:
+	
+	/opt/digiverso/goobi/config/
+	/opt/digiverso/g2g/goobi/config/
+	
+Den Regelsatz `ruleset.xml` in den Regelsatz-Ordner von Goobi kopieren, also üblicherweise:
+
+	/opt/digiverso/goobi/rulesets/
+	/opt/digiverso/g2g/goobi/rulesets/
+
+Die Dateien `predict_cnn.pypredict_cnn.py`, `issue_model_0.01-symbol.json` und `issue_model_0.01-0009.params`in den Scripts-Ordner kopieren, also üblicherweise nach:
+
+	/opt/digiverso/goobi/scripts/
+	/opt/digiverso/g2g/goobi/scripts/
+
+
+## Workflow aufsetzen und konfigurieren
+
+Es muss einen Arbeitsschritt geben, in dem das python-Script aufgerufen wird und dem die richtigen Parameter übergeben werden. Der Aufruf sieht im Allgeimenen in Goobi so aus:
+
+	/bin/bash -c "/usr/local/bin/python2 {scriptsFolder}predict_cnn.py {origpath} {imagepath}/thumbs_cnn {processpath}/taskmanager/issues_result.json {scriptsFolder}issue_model_0.01 2>/dev/null"
+	
+Im nachfolgenden Arbeitsschritt wird das richtige Plugin eingebunden, so dass es für ein manuelles Betreten verfügbar ist:
+
+	intranda_step_newspaperRecognizer	
+
+## Workflow testen
+
+Nun einen neuen Vorgang für den Workflow anlegen und dabei beachten, dass es sich bei dem Publikationstyp um eine Zeitung handeln muss. Andernfalls wird die Generierung der METS-Datei nicht wie gewünscht funktionieren.
+	
--- goobi-plugin-step-olr-newspaper-recognizer/doc/predict_cnn.py
+++ goobi-plugin-step-olr-newspaper-recognizer/doc/predict_cnn.py
@@ -5,10 +5,11 @@ import sys
 import os
 import json
 
-if len(sys.argv) > 3:
+if len(sys.argv) > 4:
     dirName = sys.argv[1]
     tempDirName = sys.argv[2]
     resultFile = sys.argv[3]
+    modelFile = sys.argv[4]
 
     if not os.path.exists(tempDirName):
         os.makedirs(tempDirName)
@@ -23,7 +24,8 @@ if len(sys.argv) > 3:
         i=0
         for f in files:
            print 'convert {0}/{1} -resize 500 -crop 500x500+0+0 {2}/{3}'.format(dirName, f, tempDirName, f.replace(".tif", ".jpg"))
-           os.system('convert {0}/{1} -resize 500 -crop 500x500+0+0 {2}/{3}'.format(dirName, f, tempDirName, f.replace(".tif", ".jpg")))
+           #os.system('convert {0}/{1} -resize 500 -crop 500x500+0+0 {2}/{3}'.format(dirName, f, tempDirName, f.replace(".tif", ".jpg")))
+           os.system('/usr/local/bin/convert {0}/{1} -resize 500 -crop 500x500+0+0 {2}/{3}'.format(dirName, f, tempDirName, f.replace(".tif", ".jpg")))
            lstFile.write(str(i) + "\t" + "0.0000\t" + f.replace(".tif",".jpg") + "\n")
            i+=1
 else:
@@ -34,7 +36,7 @@ else:
 
 data_iter = mx.image.ImageIter(batch_size=1, data_shape=(3, 50, 50), resize=75, path_imglist=tempDirName + "/images.lst", path_root=tempDirName+"/")
 
-model = mx.mod.Module.load("issue_model_0.01", 9)
+model = mx.mod.Module.load(modelFile, 9)
 model.bind(data_shapes=data_iter.provide_data, label_shapes=data_iter.provide_label)
 
 results = []
@@ -43,6 +45,10 @@ for pred, i_batch, batch in model.iter_predict(data_iter):
     nparr = pred[0].asnumpy()
     result = nparr[0][0] - nparr[0][1]
     results.append({"filename": files[i_batch], "result": float(result)})
+
+dirname=os.path.dirname
+if not os.path.exists(dirname(resultFile)):
+    os.makedirs(dirname(resultFile))
 
 with open(resultFile, "w") as outfile:
     json.dump(results, outfile)
